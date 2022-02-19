238f49f0df314df498ba56d7f8e78ae41ffb2ca5
sunilgr84 <40975161+sunilgr84@users.noreply.github.com>
2022-02-19 17:26:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyFirstApp
+# MyAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.7.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/MyFirstApp'),
+      dir: require('path').join(__dirname, './coverage/MyAngular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
